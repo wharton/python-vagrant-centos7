3e030db90d5a1dae41c015c70b8f5860cbb183e6
Allen, Timothy <tallen@wharton.upenn.edu>
2017-04-04 13:11:12 +0000

    Update PostgreSQL install script to be 9.6.

--- examples/install_postgres.sh
+++ examples/install_postgres.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
-# This script will install and enable PostgreSQL 9.5 for CentOS 7 locally
+# This script will install and enable PostgreSQL 9.6 for CentOS 7 locally
 # for development. Must be sudo'd.
 #
 
-echo "Adding the PostgreSQL 9.5 CentOS repository..."
-rpm -Uvh http://yum.postgresql.org/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm
+echo "Adding the PostgreSQL 9.6 CentOS repository..."
+rpm -Uvh http://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
 
 echo "Installing PostgreSQL..."
-yum -y --quiet install postgresql95-server postgresql95 postgresql95-devel postgresql95-contrib
-/usr/pgsql-9.5/bin/postgresql95-setup initdb
-cp /vagrant/examples/pg_hba.conf /var/lib/pgsql/9.5/data
-cp /vagrant/examples/postgresql.conf /var/lib/pgsql/9.5/data
-systemctl restart postgresql-9.5
-systemctl enable postgresql-9.5
+yum -y --quiet install postgresql96-server postgresql96 postgresql96-devel postgresql96-contrib
+/usr/pgsql-9.6/bin/postgresql96-setup initdb
+cp /vagrant/examples/pg_hba.conf /var/lib/pgsql/9.6/data
+cp /vagrant/examples/postgresql.conf /var/lib/pgsql/9.6/data
+systemctl restart postgresql-9.6
+systemctl enable postgresql-9.6
 
 echo "Opening up port 5432 on the firewall..."
 firewall-cmd --permanent --add-service postgresql > /dev/null 2>&1
