025f607c1792aa0922e07b812b38f92deaa70bb1
Allen, Timothy <tallen@wharton.upenn.edu>
2017-07-13 09:33:33 -0400

    Modify PostgreSQL installation script.

--- assets/install_postgres.sh
+++ assets/install_postgres.sh
@@ -5,22 +5,22 @@
 #
 
 echo "Adding the PostgreSQL 9.6 CentOS repository..."
-rpm -Uvh http://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+rpm --quiet -Uvh http://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm > /dev/null
 
 echo "Installing PostgreSQL..."
-yum -y --quiet install postgresql96-server postgresql96 postgresql96-devel postgresql96-contrib
-/usr/pgsql-9.6/bin/postgresql96-setup initdb
+yum --quiet -y install postgresql96-server postgresql96 postgresql96-devel postgresql96-contrib > /dev/null
+/usr/pgsql-9.6/bin/postgresql96-setup initdb > /dev/null
 cp /vagrant/assets/pg_hba.conf /var/lib/pgsql/9.6/data
 cp /vagrant/assets/postgresql.conf /var/lib/pgsql/9.6/data
 systemctl restart postgresql-9.6
 systemctl enable postgresql-9.6
-ln -s /usr/pgsql-9.6/bin/pg_config /usr/bin/pg_config
+ln -fs /usr/pgsql-9.6/bin/pg_config /usr/bin/pg_config 2>&1
 
 echo "Opening up port 5432 on the firewall..."
 firewall-cmd --permanent --add-service postgresql > /dev/null 2>&1
 systemctl restart firewalld > /dev/null 2>&1
 
-echo "Making user 'vagrant' a superuser..."
+echo "Creating superuser 'vagrant' (it may already exist)..."
 sudo -u postgres createuser -sd vagrant
 sudo -u postgres createdb vagrant
 
